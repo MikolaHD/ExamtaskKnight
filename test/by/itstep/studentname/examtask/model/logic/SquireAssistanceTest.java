d793e10e165607fc84219717b12511996a2b5ae6
MikolaHD <101862298+MikolaHD@users.noreply.github.com>
2022-04-09 21:17:54 +0300

    good

--- test/by/itstep/studentname/examtask/model/logic/SquireAssistanceTest.java
+++ test/by/itstep/studentname/examtask/model/logic/SquireAssistanceTest.java
@@ -3,23 +3,55 @@ package by.itstep.studentname.examtask.model.logic;
 import by.itstep.studentname.examtask.model.entity.Bevor;
 import by.itstep.studentname.examtask.model.entity.Helmet;
 import by.itstep.studentname.examtask.model.entity.Knight;
+
 import static org.junit.Assert.*;
+
+import by.itstep.studentname.examtask.model.entity.Poleyn;
+
 import org.junit.Test;
 
 public class SquireAssistanceTest {
 
+
     @Test
 
-    public   void testCalcTotalPricePositive(){
-        double expected = 10;
-
+    public void testCalcTotalPricePositive() {
         Knight knight = new Knight();
-
-        knight.add(new Bevor(5.3, 0, 21));
-        knight.add(new Helmet(-1, 2, 3.222222));
-
+        knight.add(new Poleyn(5.2, 5, 5));
+        knight.add(new Bevor(3.3, 5, 5));
+        knight.add(new Helmet(1.5, 5, 5));
+        double expected = 10;
         double actual = SquireAssistance.calcTotalPrice(knight);
-        assertEquals(expected,actual, 0);
+        assertEquals(expected, actual, 0);
 
     }
+
+    @Test
+
+    public void testCalcTotalWeightPositive() {
+        Knight knight = new Knight();
+        knight.add(new Poleyn(5.2, 5, 5));
+        knight.add(new Bevor(3.3, 5, 5));
+        knight.add(new Helmet(1.5, 5, 5));
+        double expected = 15;
+        double actual = SquireAssistance.calcTotalWeight(knight);
+
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+
+    public void testCalcTotalArmorPositive() {
+        Knight knight = new Knight();
+        knight.add(new Poleyn(5.2, 5, 4));
+        knight.add(new Bevor(3.3, 5, 5));
+        knight.add(new Helmet(1.5, 5, 5));
+        double expected = 14;
+        double actual = SquireAssistance.calcTotalArmor(knight);
+
+        assertEquals(expected, actual, 0);
+
+    }
+
+
 }
